7499981d2a07a2b4659c59b2ed8dd77e38ff15df
Don Jasa <LyndonJ@magenic.com>
2020-04-28 18:19:39 +0800

    completed the status screen

--- src/components/character/CharacterStats.vue
+++ src/components/character/CharacterStats.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="character-stats-container">
     <div class="character-stats row nomargin">
-      <div class="col-sm-3 nopadding">
-        Character Image Here
+      <div class="col-sm-3 nopadding center">
+        <img class="character-image" :src="characterImage" />
       </div>
       <div class="col-sm-9">
         <div class="center">Character Stats</div>
@@ -21,10 +21,55 @@
         </div>
       </div>
     </div>
+
+    <div class="character-arsenal row nomargin">
+      <div class="col-sm-6 nopadding">
+        <div class="center bold">Equipment</div>
+        <div class="row nomargin">
+          <div class="col-sm-10 offset-sm-2 bold">Weapon</div>
+          <div class="col-sm-8 offset-sm-4">{{ weapon.name }}</div>
+
+          <div class="col-sm-10 offset-sm-2 bold">Armor</div>
+          <div class="col-sm-8 offset-sm-4">{{ armor.name }}</div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 nopadding">
+        <div class="center bold">Skills</div>
+        <div class="row nomargin" v-for="skill in character.skills" :key="skill._id">
+          <div class="col-sm-8 offset-sm-2">{{ skill.name }}</div>
+          <div class="col-sm-2 bold">({{ skill.type }})</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="character-stats-actions">
+      <router-link
+        to="/character/inventory"
+        v-slot="{ href, route, navigate }"
+      >
+        <button 
+          :href="href" @click="navigate" class="btn-principal">
+          Inventory
+        </button>
+      </router-link>
+
+      <router-link
+        to="/character/skills"
+        v-slot="{ href, route, navigate }"
+      >
+        <button 
+          :href="href" @click="navigate" class="btn-principal">
+          Skills
+        </button>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import { classTypes } from "../../shared/class-types";
+
 export default {
   props: {
     character: {
@@ -33,8 +78,8 @@ export default {
   },
   computed: {
     bonusStats: function() {
-      const weapon = this.character.equipment.weapon.bonus;
-      const armor = this.character.equipment.armor.bonus;
+      const weapon = this.weapon.bonus;
+      const armor = this.armor.bonus;
 
       const bonus = {};
       this.statKeys.forEach(key => {
@@ -55,6 +100,17 @@ export default {
       });
 
       return totalStats;
+    },
+    characterImage: function() {
+      const classType = classTypes.find(x => x.id == this.character.classType);
+
+      return classType.image;
+    },
+    weapon: function() {
+      return this.character.equipment.weapon;
+    },
+    armor: function() {
+      return this.character.equipment.armor;
     }
   }
 }
@@ -68,5 +124,24 @@ export default {
   border-radius: 8px;
   padding: 2px;
   font-size: 14px;
+
+  .character-stats {
+    margin-top: 25px !important;
+
+    .character-image {
+      max-width: 90%;
+    }
+  }
+
+  .character-arsenal {
+    margin-top: 25px !important;
+    height: 150px;
+  }
+
+  .character-stats-actions {
+    text-align: right;
+    font-family: AtariClassic;
+    padding-right: 20px;
+  }
 }
 </style>
