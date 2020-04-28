7c2076090f88968308bf4448e5a26ef7db1e990e
Don Jasa <LyndonJ@magenic.com>
2020-04-28 23:20:03 +0800

    modified current prod API link

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const enrivonment = {
     apiUrl: "http://localhost:3000"
   },
   production: {
-    apiUrl: "https://monster-slayer-api.herokuapp.com"
+    apiUrl: "https://monster-slayer-api-staging.herokuapp.com"
   }
 }
 
