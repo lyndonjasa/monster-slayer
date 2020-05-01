9e1dbee5cdff4aacbdea0e186d24c3bf5ca6de6f
Don Jasa <LyndonJ@magenic.com>
2020-05-01 14:08:14 +0800

    added loading of combatant images

---
+++ src/components/dungeon/battlefield/Combatant.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="combatant-container">
+    <div class="status-bar-container">
+    </div>
+
+    <div class="character-image-container">
+      <div class="character-image-container">
+        <img class="character-image" 
+            :src="!combatant.showAlt ? combatant.image : combatant.altImage"
+            :class="{ 'player-image' : combatant.isPlayer, 'enemy-image': !combatant.isPlayer }"
+            :style="{ left: leftAlign }"
+        />
+        <img class="character-image-alt" 
+            :src="combatant.altImage"
+            style="display: none"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    combatant: { required: true }
+  },
+  data: function() {
+    return {
+      leftAlign: "0px"
+    }
+  },
+  computed: {
+    remainingHealthPercentage: function() {
+      return (this.combatant.stats.health / this.combatant.stats.totalHealth);
+    },
+    remainingManaPercentage: function() {
+      return (this.combatant.stats.mana / this.combatant.stats.totalMana);
+    }
+  },
+  mounted: function() {
+    this.$nextTick(() => {
+      const img = new Image();
+      img.src = this.combatant.image;
+
+      img.onload = () => {
+        const selector = this.combatant.isPlayer ? ".player-image" : ".enemy-image";
+        const image = document.querySelector(selector);
+        const leftPosition = (400 - image.clientWidth) / 2;
+        this.leftAlign = leftPosition + "px";
+      };
+    });
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.character-image-container {
+  text-align: center;
+  height: 327px;
+}
+
+.character-image {
+  position: absolute;
+  bottom: 70px;
+
+  &.player-image {
+    max-height: 75px;
+  }
+
+  &.enemy-image {
+    max-height: 250px;
+  }
+}
+</style>
--- src/components/dungeon/battlefield/MainBattle.vue
+++ src/components/dungeon/battlefield/MainBattle.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="main-battle-container">
-    <app-battle-stage :image="dungeon.image"></app-battle-stage>
-    Main Battle Container
+    <app-battle-stage v-if="dungeon" :image="dungeon.image">
+      <div class="dungeon-field row nomargin">
+        <div class="col-sm-6 nopadding">
+          <app-combatant :combatant="player"></app-combatant>
+        </div>
+        <div class="col-sm-6 nopadding">
+          <app-combatant :combatant="enemy"></app-combatant>
+        </div>
+      </div>
+    </app-battle-stage>
   </div>
 </template>
 
 <script>
 import BattleStage from "./BattleStage";
+import Combatant from "./Combatant";
 
 export default {
   components: {
-    appBattleStage: BattleStage
+    appBattleStage: BattleStage,
+    appCombatant: Combatant
   },
   props: {
     dungeon: { required: true },
@@ -23,5 +33,10 @@ export default {
 <style lang="scss" scoped>
 .main-battle-container {
   height: 100%;
+
+  .dungeon-field {
+    width: 100%;
+    height: 400px;
+  }
 }
 </style>
